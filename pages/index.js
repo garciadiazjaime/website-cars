b50ff460f4224315b632e2071f5494223b6acca3
jaime <garciadiazjaime@gmail.com>
2023-08-19 19:53:10 -0500

    feat: remove antd

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from "next/head";
 import { useEffect, useState, useRef } from "react";
-import { SearchOutlined } from "@ant-design/icons";
-import { Table, Input, Space, Button } from "antd";
-import Highlighter from "react-highlight-words";
 import TagManager from "react-gtm-module";
 
 const formatter = new Intl.NumberFormat("en-US", {
@@ -19,139 +16,6 @@ export const getStaticProps = async () => {
 };
 
 export default function Home({ cars }) {
-  const [searchText, setSearchText] = useState("");
-  const [searchedColumn, setSearchedColumn] = useState("");
-  const searchInput = useRef(null);
-
-  const handleSearch = (selectedKeys, confirm, dataIndex) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
-  };
-
-  const handleReset = (clearFilters) => {
-    clearFilters();
-    setSearchText("");
-  };
-
-  const getColumnSearchProps = (dataIndex) => ({
-    filterDropdown: ({
-      setSelectedKeys,
-      selectedKeys,
-      confirm,
-      clearFilters,
-      close,
-    }) => (
-      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) =>
-            setSelectedKeys(e.target.value ? [e.target.value] : [])
-          }
-          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-          style={{ marginBottom: 8, display: "block" }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-            icon={<SearchOutlined />}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Reset
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              confirm({ closeDropdown: false });
-              setSearchText(selectedKeys[0]);
-              setSearchedColumn(dataIndex);
-            }}
-          >
-            Filter
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              close();
-            }}
-          >
-            close
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered) => (
-      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
-    ),
-    onFilter: (value, record) =>
-      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
-    onFilterDropdownOpenChange: (visible) => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100);
-      }
-    },
-    render: (text, record) =>
-      searchedColumn === dataIndex ? (
-        <a href={record.link} target="_blank" style={{ color: "black" }}>
-          <Highlighter
-            highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
-            searchWords={[searchText]}
-            autoEscape
-            textToHighlight={text ? text.toString() : ""}
-          />
-        </a>
-      ) : (
-        <a href={record.link} target="_blank" style={{ color: "black" }}>
-          {text}
-        </a>
-      ),
-  });
-
-  const columns = [
-    {
-      title: "#",
-      render: (_, _record, index) => index + 1,
-    },
-    {
-      title: "Title",
-      dataIndex: "title",
-      ...getColumnSearchProps("title"),
-    },
-    {
-      title: "Price",
-      dataIndex: "price",
-      sorter: (a, b) => a.price - b.price,
-      render: (price) => formatter.format(price),
-    },
-    {
-      title: "Year",
-      dataIndex: "year",
-      sorter: (a, b) => a.year - b.year,
-    },
-    {
-      title: "VIN",
-      dataIndex: "vin",
-    },
-    {
-      title: "Mileage",
-      dataIndex: "mileage",
-      sorter: (a, b) => a.mileage - b.mileage,
-    },
-  ];
-
   const initGA = () => {
     TagManager.initialize({
       gtmId: "GTM-527VHWHN",
@@ -165,31 +29,33 @@ export default function Home({ cars }) {
   return (
     <div>
       <Head>
-        <title>Cars</title>
+        <title>Volkswagen Chicago</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>Cars</h1>
-        <Table
-          rowKey="vin"
-          columns={columns}
-          dataSource={cars}
-          size="large"
-          pagination={{ pageSize: 100 }}
-        />
+        <h1>Volkswagen Chicago</h1>
+        {cars
+          .sort((a, b) => a.price - b.price)
+          .map((car, index) => (
+            <div
+              key={car.vin}
+              style={{
+                display: "flex",
+                borderBottom: "1px solid black",
+                padding: 20,
+                display: "flex",
+              }}
+            >
+              <div style={{ flex: 1 }}>{index + 1}</div>
+              <div style={{ flex: 1 }}>{car.title}</div>
+              <div style={{ flex: 1 }}>{formatter.format(car.price)}</div>
+              <div style={{ flex: 1 }}>{car.year}</div>
+              <div style={{ flex: 1 }}>{car.mileage}</div>
+              <div style={{ flex: 1 }}>{car.vin}</div>
+            </div>
+          ))}
       </main>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-      `}</style>
 
       <style jsx global>{`
         html,
